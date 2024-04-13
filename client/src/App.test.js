f990f67b40da90f1ecb6c968bc9cb02d982bba78
kodega2016 <khadgalovecoding2016@gmail.com>
2024-04-13 17:59:40 +0545

    fix path

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,1 +1,13 @@
-test("renders learn react link", () => {});
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+
+test("renders learn react link", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Explore Docker/i);
+  expect(linkElement).toBeInTheDocument();
+});
+test("renders learn react link II", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Explore Docker/i);
+  expect(linkElement).toBeInTheDocument();
+});
